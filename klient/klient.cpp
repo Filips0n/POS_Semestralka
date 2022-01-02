afd6eed38f4eb295b49593ddb8da3261544a4b96
Filipson <filip.sudora@gmail.com>
2022-01-02 19:28:20 +0100

    Added Login, Registration

--- klient/klient.cpp
+++ klient/klient.cpp
@@ -7,26 +7,70 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+#include <string>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+typedef struct socketData{
+    char* server;
+    int socketNumber;
+    int socket;
+} DATA;
+
+int prihlasenie(void *data){
+    DATA *d = (DATA *)data;
+    char buffer[256];
+    int n;
+    printf("Please enter a name and password: ");
+    bzero(buffer,256);
+    fgets(buffer, 255, stdin);
+    n = write(d->socket, buffer, strlen(buffer));
+    if (n < 0){perror("Error writing to socket");return 5;}
+
+    bzero(buffer,256);
+    n = read(d->socket, buffer, 255);
+    if (n < 0){perror("Error reading from socket");return 6;}
+
+    if(strcmp("1", buffer)==0){
+        cout << "Uspesne prihlaseny" << endl;
+    } else if(strcmp("2", buffer)==0){
+        cout << "Zle zadane heslo" << endl;
+    } else {
+        cout << "Zadany pouzivatel neexistuje" << endl;
+    }
+}
+
+int registracia(void *data){
+    DATA *d = (DATA *)data;
+    char buffer[256];
+    int n;
+    printf("Please enter a name and password: ");
+    bzero(buffer,256);
+    fgets(buffer, 255, stdin);
+
+    n = write(d->socket, buffer, strlen(buffer));
+    if (n < 0){perror("Error writing to socket");return 5;}
+
+    bzero(buffer,256);
+    n = read(d->socket, buffer, 255);
+    if (n < 0){perror("Error reading from socket");return 6;}
+
+    if(strcmp("1", buffer)==0){
+        cout << "Uspesne zaregistrovany" << endl;
+    } else if(strcmp("2", buffer)==0){
+        cout << "Pouzivatel uz je zaregistrovany" << endl;
+    }
+}
+
+int zaregistrujSpojenie(void *data){
+    DATA *d = (DATA *)data;
     int sockfd, n;
     struct sockaddr_in serv_addr;
     struct hostent* server;
 
-    char buffer[256];
-
-    if (argc < 3)
-    {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        return 1;
-    }
-
-    server = gethostbyname(argv[1]);
-    if (server == NULL)
-    {
-        fprintf(stderr, "Error, no such host\n");
-        return 2;
-    }
+    server = gethostbyname(d->server);
+    if (server == NULL){fprintf(stderr, "Error, no such host\n");return 2;}
 
     bzero((char*)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
@@ -35,42 +79,56 @@ int main(int argc, char *argv[])
             (char*)&serv_addr.sin_addr.s_addr,
             server->h_length
     );
-    serv_addr.sin_port = htons(atoi(argv[2]));
+    serv_addr.sin_port = htons(d->socketNumber);//socket number
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        perror("Error creating socket");
-        return 3;
-    }
+    if (sockfd < 0){perror("Error creating socket");return 3;}
 
     if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        perror("Error connecting to socket");
-        return 4;
+    {perror("Error connecting to socket");return 4;}
+    d->socket = sockfd;
+}
+
+int main(int argc, char *argv[])
+{
+    DATA d = {argv[1], atoi(argv[2]), NULL};
+    zaregistrujSpojenie(&d);
+    string zaregistrovany;
+    //do {
+        cout << "Zaregistrovany pouzivatel (a/n): ";
+        cin >> zaregistrovany;
+        //flush buffer
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        int n;
+        char buffer[256];
+        bzero(buffer,256);
+        if (zaregistrovany == "a"){
+            buffer[0] = 'a';
+            n = write(d.socket, buffer, strlen(buffer));
+            if (n < 0){perror("Error writing to socket");return 5;}
+
+            prihlasenie(&d);
+        } else if(zaregistrovany == "n"){
+            buffer[0] = 'n';
+            n = write(d.socket, buffer, strlen(buffer));
+            if (n < 0){perror("Error writing to socket");return 5;}
+
+            registracia(&d);
+        } else {
+            cout << "Zadana nespravna moznost" << endl;
+        }
+    //} while(zaregistrovany != "n" || zaregistrovany != "a");
+    int volba;
+    cout << "------------------------------------------------------" << endl;
+    cout << "1 - Zrusenie uctu" << endl;
+    cin >> volba;
+    if (volba == 1) {
+        bzero(buffer,256);
+        buffer[0] = '1';
+        n = write(d.socket, buffer, strlen(buffer));
+        if (n < 0){perror("Error writing to socket");return 5;}
     }
-
-    printf("Please enter a message: ");
-    bzero(buffer,256);
-    fgets(buffer, 255, stdin);
-
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0)
-    {
-        perror("Error writing to socket");
-        return 5;
-    }
-
-    bzero(buffer,256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0)
-    {
-        perror("Error reading from socket");
-        return 6;
-    }
-
-    printf("%s\n",buffer);
-    close(sockfd);
-
+    close(d.socket);
     return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -6,16 +6,105 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-    int sockfd, newsockfd;
-    socklen_t cli_len;
-    struct sockaddr_in serv_addr, cli_addr;
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string, string> udaje;
+void vypis() {
+    for (auto x : udaje)
+        cout << x.first << " " << x.second << endl;
+    cout << "--------------------" << endl;
+}
+void nacitajUdaje(){
+    udaje.clear();
+    ifstream infile("../udaje.txt");
+    string readLine;
+    if (!infile.is_open()) { std::exit(EXIT_FAILURE); }
+    while (getline(infile, readLine)) {
+        string meno;
+        string heslo;
+        stringstream line(readLine);
+        getline(line, meno, ' ');
+        getline(line, heslo, ' ');
+        udaje[meno]=heslo;
+    }
+    infile.close();
+    vypis();
+}
+
+int zaregistrujPouzivatela(int *newsockfd){
     int n;
     char buffer[256];
+    bzero(buffer,256);
+    const char* msg = "1";
+    bool existuje = false;
+    n = read(*newsockfd, buffer, 255);
+    if (n < 0){perror("Error reading from socket");return 4;}
 
-    if (argc < 2)
-    {
+    const char * meno = strtok(buffer," ");;
+    const char * heslo = strtok(NULL,"\n");
+
+    for (auto x : udaje) {
+        if (x.first == meno){
+            msg = "2";
+            existuje = true;
+            break;
+        }
+    }
+    if (!existuje){
+        ofstream myfile;
+        myfile.open("../udaje.txt",ios::app);
+        if (myfile.is_open())
+        {
+            myfile << meno << " " << heslo << "\n";
+            myfile.close();
+        } else cout << "Unable to open file";
+        nacitajUdaje();
+    }
+
+    n = write(*newsockfd, msg, strlen(msg)+1);
+    if (n < 0){perror("Error writing to socket");return 5;}
+}
+
+int skontrolujPrihlasenie(int *newsockfd){
+    int n;
+    char buffer[256];
+    bzero(buffer,256);
+    const char* msg = "3";
+    n = read(*newsockfd, buffer, 255);
+    if (n < 0){perror("Error reading from socket");return 4;}
+
+    const char * token = strtok(buffer," ");;
+    const char * token2 = strtok(NULL,"\n");
+
+    auto item = udaje.find(token);
+    if(item != udaje.end()){
+        if(strcmp((item->second).c_str(), token2)==0){
+            msg = "1";
+        } else {
+            msg = "2";
+        }
+    } else {
+        msg = "3";
+    }
+    n = write(*newsockfd, msg, strlen(msg)+1);
+    if (n < 0){perror("Error writing to socket");return 5;}
+
+}
+
+int main(int argc, char *argv[])
+{
+    nacitajUdaje();
+    /*-------------------------------------------*/
+    int sockfd, newsockfd, n;
+    socklen_t cli_len;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    if (argc < 2) {
         fprintf(stderr,"usage %s port\n", argv[0]);
         return 1;
     }
@@ -26,45 +115,34 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(atoi(argv[1]));
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        perror("Error creating socket");
-        return 1;
-    }
+    if (sockfd < 0){perror("Error creating socket");return 1;}
 
     if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        perror("Error binding socket address");
-        return 2;
-    }
+    {perror("Error binding socket address");return 2;}
 
     listen(sockfd, 5);
     cli_len = sizeof(cli_addr);
 
     newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
-    if (newsockfd < 0)
-    {
-        perror("ERROR on accept");
-        return 3;
-    }
-
+    if (newsockfd < 0){perror("ERROR on accept");return 3;}
+    /*-------------------------------------------*/
+    char buffer[256];
     bzero(buffer,256);
     n = read(newsockfd, buffer, 255);
-    if (n < 0)
-    {
-        perror("Error reading from socket");
-        return 4;
+    if (n < 0){perror("Error reading from socket");return 4;}
+    if (strcmp(buffer, "a")==0){
+        cout << "Prihlasovanie" << endl;
+        skontrolujPrihlasenie(&newsockfd);
+    } else if(strcmp(buffer, "n")==0){
+        cout << "Registracia" << endl;
+        zaregistrujPouzivatela(&newsockfd);
     }
-    printf("Here is the message: %s\n", buffer);
-
-    const char* msg = "I got your message";
-    n = write(newsockfd, msg, strlen(msg)+1);
-    if (n < 0)
-    {
-        perror("Error writing to socket");
-        return 5;
+    /*--------------------------------------------*/
+    bzero(buffer,256);
+    n = read(newsockfd, buffer, 255);
+    if (strcmp(buffer, "1")==0){
+        zrusUcet();
     }
-
     close(newsockfd);
     close(sockfd);
 
